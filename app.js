5182e636aca3a39396ed58d3614d97653b9bd821
AlexDiNobile <101683113+AlexDiNobile@users.noreply.github.com>
2025-02-26 21:44:56 -0500

    Player Movement and Hit Detection

--- app.js
+++ app.js
@@ -21,29 +21,58 @@ app.get( '/3D', function( req, res ){
 
 //socket.io stuff
 //https://socket.io/docs/v3/emit-cheatsheet/
+
+let playerNum = 0;
+
 io.on('connection', (socket) => {
     console.log( socket.id + " connected" );
 
+    if(playerNum == 0){
+        console.log( socket.id + " Is Player One" );
+        socket.id = "playerOne";
+        console.log(socket.id);
+        playerNum = 1;
+    }
+    else if(playerNum == 1){
+        console.log( socket.id + " Is Player Two" );
+        socket.id = "playerTwo";
+        console.log(socket.id);
+        playerNum = 2;
+    }
+    if(socket.id === "playerOne"){
+        socket.on("player1Pos", (data) => {
+            //console.log( "Player One Present" );
+            io.emit("player1_update", {xPos:data.xPos, yPos:data.yPos, zPos:data.zPos, xRot:data.xRot, yRot:data.yRot, zRot:data.zRot});
+        });
+    }
+    else if(socket.id === "playerTwo"){
+        socket.on("player2Pos", (data) => {
+            //console.log( "Player Two Present" );
+            io.emit("player2_update", {xPos:data.xPos, yPos:data.yPos, zPos:data.zPos, xRot:data.xRot, yRot:data.yRot, zRot:data.zRot});
+        });
+    }
+
     socket.on('disconnect', () => {
-        console.log( socket.id + " disconnected" );
+        if(socket.id === "playerOne"){
+            console.log( socket.id + " disconnected" );
+            playerNum = 0;
+        }
+        else if(socket.id === "playerTwo"){
+            console.log( socket.id + " disconnected" );
+            playerNum = 0;
+        }
     });
 
-    socket.on("red", (data) => {
-        console.log( "red event received" );
-        io.emit("color_change", {r:255, g:0, b:0});         //to all connected clients
+    socket.on("tealHit", (data) => {
+        console.log( "player 1 hit" );
+        io.emit("pink_wins", {r:255, g:0, b:0});         //to all connected clients
         //io.socket.emit("color_change", {r:255, g:0, b:0});  //to everyone but sender
     });
 
-    socket.on("blue", (data) => {
-        console.log( "blue event received" );
-        io.emit("color_change", {r:0, g:0, b:255});
+    socket.on("pinkHit", (data) => {
+        console.log( "player 2 hit" );
+        io.emit("teal_wins", {r:0, g:0, b:255});
     });
-
-    socket.on("playersPos", (data) => {
-        console.log( "playerPos received" );
-        io.emit("player_PosRot");
-    });
-
     //question 1: how do you continuously update the network, e.g., users position and orientation?
     //question 2: how do you synch clients to current state?
 });
